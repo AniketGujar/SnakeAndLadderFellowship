b2af906385a27543f97f09433c0b58f333bdeb9a
AniketGujar <gujaraniket7875@gmail.com>
2020-12-29 12:49:25 +0530

    Added: Java File to master Branch

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,8 @@
 package Game;
 public class SnakeAndLadder {
-	static int position;
-	public SnakeAndLadder() {
-		position = 0;
-		System.out.println("Welcome to Snake and Ladder Game");
-	}
 	
 	public static void main(String[] args) {
-		SnakeAndLadder obj = new SnakeAndLadder();
-		System.out.println("Player at " + position + " position");
+		System.out.println("Welcome to Snake and Ladder Game");
 	}
 
 }
